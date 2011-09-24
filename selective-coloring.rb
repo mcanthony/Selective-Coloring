4e96e3ced400e94db6640365f965c8c62a7f6762
Nicholas Rowe <nixterrimus@gmail.com>
2011-09-24 03:08:08 -0700

    A cleaner and more concise solution

--- selective-coloring.rb
+++ selective-coloring.rb
@@ -1,53 +1,40 @@
 require 'rubygems'
 require 'chunky_png'
 
-module ChunkyPNG
-  module Color
-    def to_hsl(color)
-      color_value = Hash.new
-      color_value[:red] = r(color) / 255.0
-      color_value[:green] = g(color) / 255.0
-      color_value[:blue] = b(color) / 255.0
-      
-      lightness = (color_value.values.max + color_value.values.min)/2
-      saturation = 0
-      hue = nil
-      c = color_value.values.max - color_value.values.min
-      unless  c == 0
-        if lightness < 0.5
-          saturation = c / (color_value.values.max + color_value.values.min)
-        else
-          saturation = c / (2.0 - c)
-        end
-        case color_value.values.max
-        when color_value[:red]
-          hue = ((color_value[:green] - color_value[:blue]) / c) % 6
-        when color_value[:green]
-          hue = 2.0 + ((color_value[:blue] - color_value[:green]) / c)
-        when color_value[:blue]
-          hue = 4.0 + ((color_value[:red] - color_value[:green]) / c)
-        end
-        hue = hue * 60
-      end
-      
-      [hue, saturation*100, lightness*100]
-    end      
-  end
-  
-  module Canvas::Operations
-    def selective_color(rgb, tolerance)
-      target_hsl = ChunkyPNG::Color.to_hsl(ChunkyPNG::Color.rgb(rgb[0], rgb[1], rgb[2]))
-      output = self.grayscale
-      (0...dimension.width).each do |x|
-        (0...dimension.height).each do |y|
-          current_hsl = ChunkyPNG::Color.to_hsl(self[x,y])
-          if !current_hsl[0].nil? && ((target_hsl[0] - current_hsl[0]).abs < tolerance)
-            output[x,y] = self[x,y]
-          end
-        end
-      end
-      output
+module ChunkyPNG::Color
+  def h(color)
+    color_value = {
+      :red => r(color) / 255.0,
+      :green => g(color) / 255.0,
+      :blue => b(color) / 255.0
+    }
+    c = color_value.values.max - color_value.values.min
+    return nil if c == 0
+    case color_value.values.max
+    when color_value[:red]
+      hue = ((color_value[:green] - color_value[:blue]) / c) % 6
+    when color_value[:green]
+      hue = 2.0 + ((color_value[:blue] - color_value[:green]) / c)
+    when color_value[:blue]
+      hue = 4.0 + ((color_value[:red] - color_value[:green]) / c)
     end
+    hue * 60  
+  end
+end
+  
+module ChunkyPNG::Canvas::Operations
+  def selective_color(rgb, tolerance)
+    target_hue = ChunkyPNG::Color.h(ChunkyPNG::Color.rgb(rgb[0], rgb[1], rgb[2]))
+    output = self.grayscale
+    (0...dimension.width).each do |x|
+      (0...dimension.height).each do |y|
+        current_hue = ChunkyPNG::Color.h(self[x,y])
+        if !current_hue.nil? && ((target_hue - current_hue).abs < tolerance)
+          output[x,y] = self[x,y]
+        end
+      end
+    end
+    output
   end
 end
 
